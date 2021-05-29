7adebe2b6f976ce493a0bdf25cecf8158ad8add5
Aron <aron.1998.zeng@gmail.com>
2021-05-29 19:17:30 -0400

    Progress

--- src/api/user.js
+++ src/api/user.js
@@ -1,47 +1,6 @@
 import axios from 'axios';
 import jwt from 'jsonwebtoken';
 
-const login = function (username, password) {
-  return new Promise(function (resolve, reject) {
-    axios
-      .post(
-        'http://localhost:4000/auth/login',
-        {
-          username: username,
-          password: password,
-        },
-        { headers: { 'Content-Type': 'application/json' } }
-      )
-      // fetch('http://localhost:4000/auth/login', { mode: 'cors' })
-      .then(function (response) {
-        resolve(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-        reject(error);
-      });
-  });
-};
-
-const logout = function (token) {
-  return new Promise(function (resolve, reject) {
-    axios
-      .post(
-        'http://localhost:4000/auth/logout',
-        {},
-        {
-          headers: { 'content-Type': 'application/json', authorization: token },
-        }
-      )
-      .then((res) => {
-        resolve(res);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
-};
-
 const getUser = function (token) {
   console.log(token);
   return new Promise(function (resolve, reject) {
@@ -63,4 +22,25 @@ const getUser = function (token) {
   });
 };
 
-export { login, logout, getUser };
+const updateUser = function (token, newUser) {
+  console.log(token);
+  console.log(newUser);
+  return new Promise(function (resolve, reject) {
+    const userId = jwt.decode(token).userId;
+    axios
+      .put(`http://localhost:4000/users/${userId}`, newUser, {
+        headers: {
+          'content-Type': 'application/json',
+          authorization: token,
+        },
+      })
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+export { getUser, updateUser };
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,7 +1,29 @@
-import React, { useLayoutEffect, useState } from 'react';
-import { Table, Card, Button, Modal } from 'antd';
-import { getUser } from '../api/user';
+import React, { useContext, useLayoutEffect, useState } from 'react';
+import {
+  Table,
+  Card,
+  Button,
+  Modal,
+  Input,
+  InputNumber,
+  Select,
+  Col,
+  Row,
+} from 'antd';
+import { getUser, updateUser } from '../api/user';
 import cookies from 'js-cookie';
+import { Formik } from 'formik';
+import AuthContext from '../context/AuthContext';
+
+//TODO:
+//Create a component for a input nested in a row
+//Use the formik Field component to render the input component
+//Clean up the code a bit
+
+//Extra:
+//Styling
+
+const { Option } = Select;
 
 const columns = [
   {
@@ -32,22 +54,16 @@ const columns = [
 ];
 
 const Inventory = () => {
-  const [tab, setTab] = useState('Vegetables');
-  const [vegetables, setVegetables] = useState([]);
-  const [meat, setMeat] = useState([]);
-  const [spices, setSpices] = useState([]);
-  const [condiments, setCondiments] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { user, setUser } = useContext(AuthContext);
   const [isFormSubmitting, setIsFormSubmitting] = useState(false);
+  const [tab, setTab] = useState('vegetables');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   useLayoutEffect(() => {
     getUser(JSON.parse(cookies.get('user')).token).then((res) => {
-      console.log('res: ', res);
-      setVegetables(res.data.vegetables);
-      setMeat(res.data.meats);
-      setSpices(res.data.spices);
-      setCondiments(res.data.condiments);
+      setUser(res.data.data);
     });
-  }, [setVegetables]);
+  }, [setUser]);
   const tabList = [
     {
       key: 'Vegetables',
@@ -69,14 +85,14 @@ const Inventory = () => {
 
   const IngredientTable = ({ type }) => {
     switch (type) {
-      case 'Vegetables':
-        return <Table columns={columns} dataSource={vegetables || []} />;
-      case 'Meat':
-        return <Table columns={columns} dataSource={meat || []} />;
-      case 'Spices':
-        return <Table columns={columns} dataSource={spices || []} />;
-      case 'CondimentAndSauces':
-        return <Table columns={columns} dataSource={condiments || []} />;
+      case 'vegetables':
+        return <Table columns={columns} dataSource={user.vegetables || []} />;
+      case 'meats':
+        return <Table columns={columns} dataSource={user.meats || []} />;
+      case 'spices':
+        return <Table columns={columns} dataSource={user.spices || []} />;
+      case 'condiments':
+        return <Table columns={columns} dataSource={user.condiments || []} />;
     }
   };
 
@@ -119,7 +135,101 @@ const Inventory = () => {
         onCancel={() => {
           setIsModalOpen(false);
         }}
-      ></Modal>
+        okButtonProps={{ style: { display: 'none' } }}
+      >
+        <Formik
+          initialValues={{ name: '', amount: 0, unit: 'gram' }}
+          onSubmit={(values, { resetForm }) => {
+            console.log('here');
+            console.log(values);
+            setIsFormSubmitting(true);
+            let newUser = { ...user };
+            let newIngredient = {
+              name: values.name,
+              amount: values.amount,
+              unit: values.unit,
+            };
+            console.log('new ingredient', newIngredient);
+            switch (tab) {
+              case 'vegetables':
+                console.log('editing vegetables');
+                newUser.vegetables.push(newIngredient);
+                break;
+              case 'meats':
+                newUser.meats.push(newIngredient);
+                break;
+              case 'spices':
+                newUser.spices.push(newIngredient);
+                break;
+              case 'condiments':
+                newUser.condiments.push(newIngredient);
+                break;
+            }
+
+            updateUser(JSON.parse(cookies.get('user')).token, newUser)
+              .then((res) => {
+                setUser(res.data);
+              })
+              .catch((res) => {
+                console.log(res);
+              });
+            resetForm({ name: '', amount: 0, unit: 'gram' });
+            setIsFormSubmitting(false);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            /* and other goodies */
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <Row>
+                <Col span={12} offset={6}>
+                  <Input
+                    name="name"
+                    id="name"
+                    onBlur={handleBlur}
+                    defaultValue={values.name}
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col span={12} offset={6}>
+                  <InputNumber
+                    name="amount"
+                    id="amount"
+                    onBlur={handleBlur}
+                    defaultValue={values.amount}
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col span={12} offset={6}>
+                  <Select
+                    name="unit"
+                    id="unit"
+                    onBlur={handleBlur}
+                    defaultValue={values.unit}
+                  >
+                    <Option value="ml">ml</Option>
+                    <Option value="gram">gram</Option>
+                    <Option value="cup">cup</Option>
+                  </Select>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={12} offset={6}>
+                  <Button onClick={handleSubmit}>Save</Button>
+                </Col>
+              </Row>
+            </form>
+          )}
+        </Formik>
+      </Modal>
     </Card>
   );
 };
